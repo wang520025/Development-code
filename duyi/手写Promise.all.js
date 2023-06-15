9b9521ad44c16ad664cfae957bc3731f1a21f52c
wang520025 <2321382408@qq.com>
2023-06-15 17:54:36 +0800

    锥形渐变

--- duyi/手写Promise.all.js
+++ duyi/手写Promise.all.js
@@ -29,6 +29,25 @@ Promise.myAll = function (proms) {
     return p;
 };
 
-Promise.myAll([1, 2, 3]).then((datas) => {
-    console.log(datas);
-});
+// Promise.myAll([1, 2, 3]).then((datas) => {
+//     console.log(datas);
+// });
+
+Promise.resolve().then(() => {
+    console.log(0)
+    return Promise.resolve(4)
+}).then((res) => {
+    console.log(res)
+})
+
+Promise.resolve().then(() => {
+    console.log(1)
+}).then(() => {
+    console.log(2)
+}).then(() => {
+    console.log(3)
+}).then(() => {
+    console.log(5)
+}).then(() => {
+    console.log(6)
+})
